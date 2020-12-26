34b4ff1b559ae8b99fb377435245c3c3e94e430c
Poscat <poscat@mail.poscat.moe>
2020-12-26 11:03:44 +0800

    Add simple error handling to auth

--- src/Web/Pixiv/Auth.hs
+++ src/Web/Pixiv/Auth.hs
@@ -3,13 +3,16 @@
 
 module Web.Pixiv.Auth where
 
+import Control.Applicative
 import Crypto.Hash.MD5
+import Data.Aeson
 import qualified Data.Aeson as A
 import Data.ByteString (ByteString)
-import Data.ByteString.Base16
+import qualified Data.ByteString.Base16 as B16
 import qualified Data.ByteString.Char8 as C
 import Data.Text (Text)
 import Data.Time
+import Deriving.Aeson (CamelToSnake, ConstructorTagModifier)
 import Deriving.Aeson.Stock
 import Network.HTTP.Client
 import Network.HTTP.Client.MultipartFormData
@@ -38,14 +41,49 @@ data OAuth2Token = OAuth2Token
   deriving (Show, Eq, Generic)
   deriving (FromJSON, ToJSON) via Snake OAuth2Token
 
-auth :: Credential -> IO (Maybe OAuth2Token)
+data Errors
+  = InvalidRequest
+  | InvalidClient
+  | InvalidGrant
+  | UnauthorizedClient
+  | UnsupportedGrantType
+  | InvalidScope
+  deriving (Show, Eq, Generic)
+  deriving (FromJSON, ToJSON) via CustomJSON '[ConstructorTagModifier CamelToSnake] Errors
+
+data OAuth2Error = OAuth2Error
+  { error :: Errors,
+    message :: Text
+  }
+  deriving (Show, Eq, Generic)
+
+instance FromJSON OAuth2Error where
+  parseJSON = withObject "oauth2 response" $ \o -> do
+    error <- o .: "error"
+    errors <- o .: "errors"
+    message <- flip (withObject "errors") errors $ \o' -> do
+      system <- o' .: "system"
+      flip (withObject "system") system $ \o'' -> do
+        o'' .: "message"
+    pure OAuth2Error {..}
+
+data OAuth2Result
+  = AuthSuccess OAuth2Token
+  | AuthFailure OAuth2Error
+  deriving (Show, Eq, Generic)
+
+instance FromJSON OAuth2Result where
+  parseJSON v =
+    AuthSuccess <$> parseJSON v <|> AuthFailure <$> parseJSON v
+
+auth :: Credential -> IO OAuth2Result
 auth Credential {..} = do
   manager <- newTlsManager
   let authUrl = "https://oauth.secure.pixiv.net/auth/token"
   initReq <- parseRequest authUrl
   utcT <- getCurrentTime
   let clientTime = C.pack $ formatTime defaultTimeLocale "%Y-%m-%dT%H:%I:%S+00:00" utcT
-      clientHash = encode $ hash $ clientTime <> hashSecret
+      clientHash = B16.encode $ hash $ clientTime <> hashSecret
       headers =
         [ ("User-Agent", "PixivAndroidApp/5.0.64 (Android 6.0)"),
           ("X-Client-Time", clientTime),
@@ -63,4 +101,4 @@ auth Credential {..} = do
   finalReq <- formDataBody parts req
   resp <- httpLbs finalReq manager
   let body = responseBody resp
-  pure $ A.decode body
+  maybe (fail "impossible: unable to parse response") pure (A.decode body)
